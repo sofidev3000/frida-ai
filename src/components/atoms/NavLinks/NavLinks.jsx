879356859f609b9296b6ab23132f2387eab1b559
SofiDev <angiemeow89@gmail.com>
2024-07-09 11:31:42 -0600

    🚧 Header panel

--- src/components/atoms/NavLinks/NavLinks.jsx
+++ src/components/atoms/NavLinks/NavLinks.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { headerMainMenu } from '../../../data/navMenu';
-import "./nav-links.css"
+import React, { useState } from "react";
+import { headerMainMenu } from "../../../data/navMenu";
+import "./nav-links.css";
 const HeaderMenu = () => {
   const [showDropdown, setShowDropdown] = useState(false);
 
@@ -14,7 +14,6 @@ const HeaderMenu = () => {
 
   return (
     <nav className="header__menu">
-      <div className='header__menu-panel'>
       <ul className="header__list">
         {headerMainMenu.map((item, index) => (
           <li
@@ -23,14 +22,14 @@ const HeaderMenu = () => {
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
           >
-            <a className="header__link" href={item.href || '#'}>
+            <a className="header__link" href={item.href || "#"}>
               {item.text}
             </a>
             {item.dropdown && showDropdown && (
               <ul className="dropdown">
                 {item.submenu.map((subItem, subIndex) => (
-                  <li key={subIndex}>
-                    <a href={subItem.link}>{subItem.text}</a>
+                  <li className="dropdown__item" key={subIndex}>
+                    <a className="dropdow__link" href={subItem.link}>{subItem.text}</a>
                   </li>
                 ))}
               </ul>
@@ -38,7 +37,6 @@ const HeaderMenu = () => {
           </li>
         ))}
       </ul>
-      </div>
     </nav>
   );
 };
