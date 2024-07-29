644f359f025f8317ffdad723e890599acaa58cd2
SofiDev <angiemeow89@gmail.com>
2024-07-29 17:07:59 -0600

    ♻️ Fixing html semantic structure

--- src/data/navMenu.js
+++ src/data/navMenu.js
@@ -1,15 +1,8 @@
 const headerMainMenu = [
-    {text:"caracteristicas", href:"#" },
-    { text: "Earnings", link: "#" },
-    {
-        text: "Dropdown",
-        dropdown: true,
-        submenu: [
-          { text: "Overview", link: "#" },
-          { text: "Overview2", link: "#" },
-          { text: "Overview3", link: "#" },
-        ],
-      },
+    {text:"Caracteristicas", href:"#caracteristicas" },
+    { text: "Recursos", link: "#" },
+    { text: "Recursos", link: "#" },
+    { text: "Para empresas", link: "#" },
 ]
 
 export  {headerMainMenu};
